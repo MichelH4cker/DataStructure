ca26c808dc45d5cbd08bedd7d6e807f39e23d509
Michel Hecker Faria <michel@pop-os.localdomain>
2021-09-23 15:57:22 -0300

    end insertion created

--- sequential-list/insertion.c
+++ sequential-list/insertion.c
@@ -3,40 +3,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int error(List *list){
     if (list == NULL || list->occupiedPositions == MAX_LIST){
+        printf("algo de errado não está certo! melhor começar tudo de novo! \n");
         return 1;
     } 
     return 0;
 }
 
-void startInsertion(List *list, struct Data contentX){
+int startInsertion(List *list, struct Data content){
     if (error(list) == 1) {
-        printf("algo de errado não está certo, melhor começar tudo de novo! \n");
+        return 0;
     } 
     
     for (int i = 0; i < list->occupiedPositions; i++){
         list->list[i + 1] = list->list[i];
-        printf("%d \n", list->list[1].dataInteger);
+        
     }
 
-    list->list[0] = contentX;
+    list->list[0] = content;
     list->occupiedPositions++;
     printf("inserido com sucesso! \n");
+    printf("\n");
 
-    
+    return 1;
 }
 
-int endInsertion(List *list){
-    if (error){
-        return (-1);
+int endInsertion(List *list, struct Data content){
+    if (error(list) == 1){
+        return 0;
     }
+    
+    list->list[list->occupiedPositions] = content;
+    list->occupiedPositions++;
+
+    return 1;
 }
 
-int specificInsertion(List *list){
+int specificInsertion(List *list, struct Data content){
     if (error){ 
-        return (-1);
+        return 0;
     }
 }
 
