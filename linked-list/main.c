2b143315b0a36a85818703bef68177462b97d397
Michel Hecker Faria <michel@pop-os.localdomain>
2021-10-03 11:55:42 -0300

    Insert function name changed

--- linked-list/main.c
+++ linked-list/main.c
@@ -4,7 +4,7 @@ int main(){
     struct Data data1 = {1, "michel", 7.5};
     List *head = createList();
     
-    pushHome(head, data1);
+    pushFront(head, data1);
 
     destroyList(head);
     return 0;
