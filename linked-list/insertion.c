7c860e7863219b51068054190d4ad08dc5e9d50d
MichelH4cker <michel.hecker@usp.br>
2021-10-04 15:13:32 -0300

    empty list verification added in insertion.c

--- linked-list/insertion.c
+++ linked-list/insertion.c
@@ -20,13 +20,16 @@ void push(struct Node **head, struct Data inputData){
 void append(struct Node **head, struct Data inputData){
     struct Node *node;
     node = malloc(sizeof(struct Node));
+    if(node == NULL){
+        return;
+    }
     node->data = inputData;
     node->next = NULL;
 
     struct Node *lastNode = *head;
-    // falta checar se a lista está vazia
-    // ou seja, se não possui head
-    if (node == NULL || head == NULL){
+    
+    if ((*head) == NULL){ // lista vazia insere no início
+        *head = node;
         return;
     } else {
         while (lastNode->next != NULL){
@@ -34,7 +37,6 @@ void append(struct Node **head, struct Data inputData){
         }
 
         lastNode->next = node;
-
         return;
     }    
 }
@@ -44,12 +46,9 @@ void sortedInsert(struct Node **head, struct Data inputData){
     struct Node *newNode;
     newNode = malloc(sizeof(struct Node));
     newNode->data = inputData;
-    
-    
+        
     struct Node *currentNode = *head;
 
-    // miss special case where only existis head
-    // or list didnt has any data
     if (*head == NULL || (*head)->data.dataInteger >= newNode->data.dataInteger ){   
         newNode->next = *head;
         *head = newNode; 
@@ -63,7 +62,6 @@ void sortedInsert(struct Node **head, struct Data inputData){
         }
         newNode->next = currentNode->next;
         currentNode->next = newNode;
+        return;
     }
-
-    return;
 }
