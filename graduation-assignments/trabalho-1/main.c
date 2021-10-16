717d77579642190c1efb746fea32798b2ba2ef5e
MichelH4cker <michel.hecker@usp.br>
2021-10-16 10:22:11 -0300

    append function created

--- graduation-assignments/trabalho-1/main.c
+++ graduation-assignments/trabalho-1/main.c
@@ -1,14 +1,16 @@
 #include "main.h"
 
-void showAllNodes(struct Node *array, int totalInputs){
+void showAllNodes(struct Node **array, int totalInputs){
     struct Node *currentNode;
+    printf("dentro da posicao zero do array, temos o float de %f \n", array[0]->next->data);
     float ithInput;
     printf("entrou no showAllNodes \n");
     for (int positionOnArray = 0; positionOnArray < totalInputs; positionOnArray++){
         printf("entrou no primeiro for! \n");
-        *currentNode = array[positionOnArray]; // receive head_ref
+        //*currentNode = array[positionOnArray]; // receive head_ref
         for (int i = 0; ithInput != 0; i++){   // run all nodes
             printf("entrou no segundo for! \n");
+            printf("valor do currentNode %f \n", currentNode->data);
             ithInput = (*currentNode).data;
             printf("Input: %f \n", ithInput);
             currentNode = (*currentNode).next;
@@ -18,23 +20,20 @@ void showAllNodes(struct Node *array, int totalInputs){
     
 
 }
-
-int receiveInputs(struct Node *array, int totalInputs){
+    
+struct Node* receiveInputs(struct Node **head){
     float input;
-    for (int positionOnArray = 0; positionOnArray < totalInputs; positionOnArray++){    // run all lists
-        struct Node *head = NULL;
-        for (int data = 0; ; data++){   // insert all values
-            scanf("%f", &input);
-            crescentInsert(&head, input);
+    struct Node *head_ref = *head;
+    for (int data = 0; ; data++){   // insert all values
+        scanf("%f", &input);
+        if (input == 0){
+            append(&head_ref, input);
+            return head_ref;
+        } else {
+            crescentInsert(&head_ref, input);
             printf("o input inserido foi %f \n", input);
-            if (input == 0){
-                array[positionOnArray] = *head;
-                break;
-            }
-            
         }
-        
-        
+    
     }
 }
 
@@ -45,11 +44,16 @@ int main (void){
     int totalInputs;
     scanf("%d", &totalInputs);
 
-    struct Node array[totalInputs];
+    struct Node* array[totalInputs];
     
     //receive inputs
-    receiveInputs(array, totalInputs);
-    printf("\n %f \n\n", array[0].next->data);
+    for (int positionOnArray = 0; positionOnArray < totalInputs; positionOnArray++){
+        struct Node *head = NULL;
+        head = receiveInputs(&head);
+        array[positionOnArray] = head;
+    }
+    
+    printf("\n %f \n\n", array[0]->next->data);
     showAllNodes(array, totalInputs);
 }
 
