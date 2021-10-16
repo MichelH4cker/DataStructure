434df851610813a251bd4403cccca4eed12c2b2d
MichelH4cker <michel.hecker@usp.br>
2021-10-16 17:22:25 -0300

    finished

--- graduation-assignments/trabalho-1/insertion.h
+++ graduation-assignments/trabalho-1/insertion.h
@@ -10,24 +10,18 @@
 struct Node;
 
 /**
- * 
- * 
- * 
+ * Insere de maneira decrescente
+ * @param head_ref primeiro nó da lista
+ * @param inputData valor a ser inserido na lista
  */
-void descendingInsert(struct Node **head, float inputData);
+void descendingInsert(struct Node **head_ref, float inputData);
 
 /**
- * 
- * 
- * 
+ * Insere de maneira crescente
+ * @param head_ref primeiro nó da lista
+ * @param inputData valor a ser inserido na lista
  */ 
-void crescentInsert(struct Node **head, float inputData);
+void crescentInsert(struct Node **head_ref, float inputData);
 
-/**
- * 
- * 
- * 
- */ 
-void append(struct Node **head, float input);
 
 #endif
