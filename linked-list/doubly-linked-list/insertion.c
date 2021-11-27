a8efb689affdfc50007b35cf4d35ea94c043c50a
MichelH4cker <michel.hecker@usp.br>
2021-11-27 18:51:42 -0300

    append and push function created

--- linked-list/doubly-linked-list/insertion.c
+++ linked-list/doubly-linked-list/insertion.c
@@ -1,19 +1,46 @@
 #include "insertion.h"
 
 void push(struct Data data, struct Node **head_ref){
-    struct Node *node;
-    node = malloc(sizeof(struct Node));
-    if (node == NULL){ // não alocado
+    struct Node *newNode;
+    newNode = malloc(sizeof(struct Node));
+    if (newNode == NULL){ // não alocado  
         return;
     }
 
-    node->data = data;
-    node->next = (*head_ref);
-    *head_ref = node;
+    newNode->data = data;                 
 
+    newNode->next = (*head_ref);          
+    newNode->previous = NULL;     
+
+    if (*head_ref != NULL){
+        (*head_ref)->previous = newNode;  
+    }
+
+    (*head_ref) = newNode;                
 }
 
 void append(struct Data data, struct Node **head_ref){
-    
+    struct Node *lastNode = *head_ref;
+    struct Node *newNode;
+    newNode = malloc(sizeof(struct Node));
+    if (newNode == NULL){ // não alocado
+        return; 
+    }
+
+    newNode->data = data;       // newNode recebe dados
+    newNode->next = NULL;       // newNode aponta para nada
+
+    if ((*head_ref) == NULL){   // condição para lista vazia
+        newNode->previous == NULL;
+        (*head_ref) = newNode;
+        return;
+    }
+
+    while (lastNode->next != NULL){
+        lastNode = lastNode->next;
+    }
+
+    lastNode->next = newNode;
+    newNode->previous = lastNode;
 
 } 
