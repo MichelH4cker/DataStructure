281ab4f789f0bdb306529a86a0ce370dad3100a7
MichelH4cker <michel.hecker@usp.br>
2021-12-14 18:15:55 -0300

    name and nusp

--- src/graduation-assignments/test-2/main.c
+++ src/graduation-assignments/test-2/main.c
@@ -1,3 +1,14 @@
+/**
+ * @file main.c
+ * @author Michel Hecker Faria - NUSP: 12609690
+ * @brief 
+ * @version 0.1
+ * @date 2021-12-14
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
 #include "main.h"
 
 int main(){
